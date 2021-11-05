8c689a8f817c1ba9971553df311267a1110c87eb
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2021-11-05 12:43:05 +0300

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,9 @@ const btn = document.querySelector('.btn');
 const content = document.querySelector('.content-container');
 
 btn.addEventListener('click', () => {
-  console.log('click');
   btn.classList.add('btn_border');
-  // btn.style.border = '10px solid #0fcf48';
-  content.classList.toggle('hidden');
+
+  content.classList.remove('hidden');
+  content.classList.toggle('close');
+  content.classList.toggle('open');
 });
